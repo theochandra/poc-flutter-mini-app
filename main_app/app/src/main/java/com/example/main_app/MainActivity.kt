af12debd8d28e0a331329deb285fc4053de162c7
theochandra <theochandra27@gmail.com>
2021-09-21 22:35:28 +0700

    successfully open main.dart from on button click in main activity

--- main_app/app/src/main/java/com/example/main_app/MainActivity.kt
+++ main_app/app/src/main/java/com/example/main_app/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.main_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.main_app.databinding.ActivityMainBinding
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnOpenFlutterModule.setOnClickListener {
+            startActivity(
+                FlutterActivity.createDefaultIntent(this)
+            )
+        }
     }
 }

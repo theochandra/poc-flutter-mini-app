c03c0902bded77d9aae3eb01cf445d2f67db7f62
theochandra <theochandra27@gmail.com>
2021-09-23 23:09:06 +0700

    get available cameras in main

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+List<CameraDescription> cameras = [];
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: Routes.initial,

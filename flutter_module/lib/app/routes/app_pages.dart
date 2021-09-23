2366b3c80f0efcd8d3ecc966c3255dbd0599d9ca
theochandra <theochandra27@gmail.com>
2021-09-23 21:30:21 +0700

    add show post routes

--- flutter_module/lib/app/routes/app_pages.dart
+++ flutter_module/lib/app/routes/app_pages.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_module/app/modules/home/home_binding.dart';
 import 'package:flutter_module/app/modules/home/home_page.dart';
+import 'package:flutter_module/app/modules/show_post/show_post_binding.dart';
+import 'package:flutter_module/app/modules/show_post/show_post_page.dart';
 import 'package:get/get.dart';
 
 part 'app_routes.dart';
@@ -10,6 +12,11 @@ class AppPages {
       name: Routes.initial,
       page: () => HomePage(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: Routes.showPost,
+      page: () => ShowPostPage(),
+      binding: ShowPostBinding(),
     ),
   ];
 }
--- flutter_module/lib/app/routes/app_routes.dart
+++ flutter_module/lib/app/routes/app_routes.dart
@@ -2,4 +2,5 @@ part of 'app_pages.dart';
 
 abstract class Routes {
   static const initial = '/';
+  static const showPost = '/showPost';
 }

8df59ab733d74f2b0732526cbfb2e12853b12f4f
theochandra <theochandra27@gmail.com>
2021-09-24 18:10:04 +0700

    add routes

--- flutter_module/lib/app/routes/app_pages.dart
+++ flutter_module/lib/app/routes/app_pages.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_module/app/modules/camera/camera_page.dart';
 import 'package:flutter_module/app/modules/home/home_binding.dart';
 import 'package:flutter_module/app/modules/home/home_page.dart';
+import 'package:flutter_module/app/modules/location/show_location_page.dart';
 import 'package:flutter_module/app/modules/show_post/show_post_binding.dart';
 import 'package:flutter_module/app/modules/show_post/show_post_page.dart';
 import 'package:get/get.dart';
@@ -22,6 +23,10 @@ class AppPages {
     GetPage(
       name: Routes.cameraPage,
       page: () => CameraPage(),
-    )
+    ),
+    GetPage(
+      name: Routes.showLocation,
+      page: () => ShowLocationPage(),
+    ),
   ];
 }
--- flutter_module/lib/app/routes/app_routes.dart
+++ flutter_module/lib/app/routes/app_routes.dart
@@ -4,4 +4,5 @@ abstract class Routes {
   static const initial = '/';
   static const showPost = '/showPost';
   static const cameraPage = '/cameraPage';
+  static const showLocation = '/showLocation';
 }

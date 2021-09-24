de5724bf3994d4a927174369c4ab41bc1f013f10
theochandra <theochandra27@gmail.com>
2021-09-25 00:38:39 +0700

    update main.dart

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:camera/camera.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_module/app/services/push_notification_service.dart';
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
@@ -8,6 +10,8 @@ List<CameraDescription> cameras = [];
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  PushNotificationService().init();
   cameras = await availableCameras();
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,

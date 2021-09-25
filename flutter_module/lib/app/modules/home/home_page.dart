52f0d04be1366793f43a5eddf4f6d90a12bf84f1
theochandra <theochandra27@gmail.com>
2021-09-25 15:25:44 +0700

    experiment close flutter module from android activity

--- flutter_module/lib/app/modules/home/home_page.dart
+++ flutter_module/lib/app/modules/home/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_module/app/global_widgets/item_view.dart';
 import 'package:flutter_module/app/global_widgets/loading_widget.dart';
 import 'package:flutter_module/app/modules/home/home_controller.dart';
 import 'package:flutter_module/app/routes/app_pages.dart';
+import 'package:flutter_module/app/services/method_channel_service.dart';
 import 'package:get/get.dart';
 
 class HomePage extends GetView<HomeController> {
@@ -11,67 +12,75 @@ class HomePage extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: Container(
-        child: GetX<HomeController>(
-          initState: (state) {
-            Get.find<HomeController>().getAll();
-          },
-          builder: (_) {
-            return _.postList.length < 1
-                ? LoadingWidget()
-                : SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(12, 8, 12, 0),
-                          child: Row(
-                            children: [
-                              GridButton(
-                                icon: Icon(
-                                  Icons.camera_alt,
-                                  color: Colors.white,
+    return WillPopScope(
+      onWillPop: MethodChannelService.closeFlutterModule(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Page'),
+          leading: Icon(
+            Icons.close,
+            color: Colors.white,
+          ),
+        ),
+        body: Container(
+          child: GetX<HomeController>(
+            initState: (state) {
+              Get.find<HomeController>().getAll();
+            },
+            builder: (_) {
+              return _.postList.length < 1
+                  ? LoadingWidget()
+                  : SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(12, 8, 12, 0),
+                            child: Row(
+                              children: [
+                                GridButton(
+                                  icon: Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.white,
+                                  ),
+                                  callback: () =>
+                                      Get.toNamed(Routes.cameraPage),
                                 ),
-                                callback: () => Get.toNamed(Routes.cameraPage),
-                              ),
-                              SizedBox(width: 12),
-                              GridButton(
-                                icon: Icon(
-                                  Icons.location_on,
-                                  color: Colors.white,
+                                SizedBox(width: 12),
+                                GridButton(
+                                  icon: Icon(
+                                    Icons.location_on,
+                                    color: Colors.white,
+                                  ),
+                                  callback: () =>
+                                      Get.toNamed(Routes.showLocation),
                                 ),
-                                callback: () =>
-                                    Get.toNamed(Routes.showLocation),
-                              ),
-                              SizedBox(width: 12),
-                              GridButton(
-                                icon: Icon(
-                                  Icons.book,
-                                  color: Colors.white,
+                                SizedBox(width: 12),
+                                GridButton(
+                                  icon: Icon(
+                                    Icons.book,
+                                    color: Colors.white,
+                                  ),
+                                  callback: () => Get.toNamed(Routes.showPost),
                                 ),
-                                callback: () => Get.toNamed(Routes.showPost),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 12),
-                          child: Divider(),
-                        ),
-                        ..._.postList.map(
-                          (model) => ItemView(
-                            title: model.title,
-                            subtitle: model.body,
-                            onTap: () {},
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12),
+                            child: Divider(),
                           ),
-                        ),
-                      ],
-                    ),
-                  );
-          },
+                          ..._.postList.map(
+                            (model) => ItemView(
+                              title: model.title,
+                              subtitle: model.body,
+                              onTap: () {},
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+            },
+          ),
         ),
       ),
     );
--- main_app/app/src/main/java/com/example/main_app/MainActivity.kt
+++ main_app/app/src/main/java/com/example/main_app/MainActivity.kt
@@ -4,15 +4,45 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.main_app.databinding.ActivityMainBinding
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private var flutterEngine: FlutterEngine? = null
+
+    private val SCREEN_ENGINE_ID = "screenEngineId"
+    private val CHANNEL = "com.example.flutter_module/closeModule"
+    private val INVOKED_METHOD = "exitFlutterModule"
+
+    private fun setUpFlutter() {
+        if (flutterEngine == null) {
+            flutterEngine = FlutterEngineCache.getInstance().get(SCREEN_ENGINE_ID)
+            flutterEngine!!
+                .dartExecutor
+                .executeDartEntrypoint(
+                    DartExecutor.DartEntrypoint.createDefault()
+                )
+        }
+
+        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call, result ->
+                when(call.method) {
+                    INVOKED_METHOD -> finish()
+                    else -> result.notImplemented()
+                }
+            }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+//        setUpFlutter()
 
         binding.btnOpenFlutterModule.setOnClickListener {
             startActivity(

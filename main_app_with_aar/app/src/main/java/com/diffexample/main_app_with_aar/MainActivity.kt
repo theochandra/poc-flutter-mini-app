ca74202e8ae81a0b4f787717355d38361870dd64
theochandra <theochandra27@gmail.com>
2021-09-26 15:33:01 +0700

    open flutter module from main activity

--- main_app_with_aar/app/src/main/java/com/diffexample/main_app_with_aar/MainActivity.kt
+++ main_app_with_aar/app/src/main/java/com/diffexample/main_app_with_aar/MainActivity.kt
@@ -2,10 +2,22 @@ package com.diffexample.main_app_with_aar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.diffexample.main_app_with_aar.databinding.ActivityMainBinding
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnOpenFlutterModule.setOnClickListener {
+            startActivity(
+                FlutterActivity.createDefaultIntent(this)
+            )
+        }
     }
 }

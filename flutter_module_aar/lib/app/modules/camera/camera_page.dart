a4a4d1bfab83a3b34e635f14bcb43ba6dcbf4c16
theochandra <theochandra27@gmail.com>
2021-09-26 13:06:17 +0700

    add camera page

---
+++ flutter_module_aar/lib/app/modules/camera/camera_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_module_aar/app/modules/camera/camera_screen.dart';
+import 'package:flutter_module_aar/main.dart';
+
+class CameraPage extends StatelessWidget {
+  const CameraPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Camera Page',
+      home: CameraScreen(cameras: cameras),
+    );
+  }
+}

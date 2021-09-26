65dfacfcf49706fd8aeeeabb3b9423a8ec72d231
theochandra <theochandra27@gmail.com>
2021-09-26 13:05:51 +0700

    add routes

---
+++ flutter_module_aar/lib/app/routes/app_pages.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_module_aar/app/modules/camera/camera_page.dart';
+import 'package:flutter_module_aar/app/modules/home/home_binding.dart';
+import 'package:flutter_module_aar/app/modules/home/home_page.dart';
+import 'package:flutter_module_aar/app/modules/location/show_location_page.dart';
+import 'package:flutter_module_aar/app/modules/show_post/show_post_binding.dart';
+import 'package:flutter_module_aar/app/modules/show_post/show_post_page.dart';
+import 'package:get/get.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  static final pages = [
+    GetPage(
+      name: Routes.initial,
+      page: () => HomePage(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: Routes.showPost,
+      page: () => ShowPostPage(),
+      binding: ShowPostBinding(),
+    ),
+    GetPage(
+      name: Routes.cameraPage,
+      page: () => CameraPage(),
+    ),
+    GetPage(
+      name: Routes.showLocation,
+      page: () => ShowLocationPage(),
+    ),
+  ];
+}
---
+++ flutter_module_aar/lib/app/routes/app_routes.dart
@@ -0,0 +1,8 @@
+part of 'app_pages.dart';
+
+abstract class Routes {
+  static const initial = '/';
+  static const showPost = '/showPost';
+  static const cameraPage = '/cameraPage';
+  static const showLocation = '/showLocation';
+}
